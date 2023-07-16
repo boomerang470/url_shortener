93e426d21c23d1e3b77a43c7c6f0e6cd05b29ef6
boomerang470 <tejthorat123@gmail.com>
2023-07-16 18:50:42 +0530

    worked on helpers and shorten

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,6 +1,10 @@
 package routes
 
-import "time"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 type request struct {
 	URL         string        `json:"url"`
@@ -14,4 +18,29 @@ type response struct {
 	Expiry          time.Duration `json:"expiry"`
 	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse json"})
+	}
+
+	//implement rate limiting
+
+	//check if input is an actual URL
+	if !govalidator.IsURL(body.URL) {
+		return c.SendStatus(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	//check for domain error
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "domain error"})
+	}
+
+	//enforce https,ssl
+
+	body.URL = helpers.EnforceHTTP(body.URL)
+
 }
